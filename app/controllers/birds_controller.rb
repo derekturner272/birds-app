4c3f16ef595c720ba7c3ba3f7f585c3fa5c8c8c4
Derek Turner <turner.derek.b@gmail.com>
2020-07-15 14:10:34 -0600

    bird-app rails api practice

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,24 @@
+class BirdsController < ApplicationController
+  
+  def index
+    @birds = Bird.all
+
+    render json: @birds
+  end
+
+  def show
+    @bird = Bird.find(params[:id])
+
+    render json: @bird
+  end
+
+  def create
+    @bird = Bird.create({
+      name:params[:name],
+      age:params[:age]
+    })
+
+    render json: @bird
+  end
+
+end
